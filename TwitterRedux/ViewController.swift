f27190d1b81d482422160337671da853893e0ff5
Deeksha Prabhakar <deekiyengar@gmail.com>
2016-11-01 12:26:05 -0700

    Initial Commit

---
+++ TwitterRedux/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterRedux
+//
+//  Created by Deeksha Prabhakar on 11/1/16.
+//  Copyright © 2016 Deeksha Prabhakar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
